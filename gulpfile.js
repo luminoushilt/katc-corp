d12210bb5114ff34fb72165bc354efc95aaa5346
Hilton Williams <hwilliams@luminoushilt.com>
2018-01-24 14:39:27 -0800

    Initial Commit after over haul.

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,26 @@
 // Plugins
 // --------------------------------------------------------------------
 
-var gulp        	= require('gulp'),
+var gulp 			= require('gulp'),
 	pug				= require('gulp-pug'),
 	sass        	= require('gulp-sass'),
-	plumber     	= require('gulp-plumber'),
-	prefix      	= require('gulp-autoprefixer'),
+	plumber 		= require('gulp-plumber'),
+	prefix 			= require('gulp-autoprefixer'),
+	image 			= require('gulp-imagemin'),
+	cleanCSS		= require('gulp-clean-css'),
+	uglify 			= require('gulp-uglify'),
+	pump			= require('pump'),
 	config 			= require('./config.json'), // external config file
 	browserSync 	= require('browser-sync').create();
 
 
 // --------------------------------------------------------------------
-// Settings
+// Path Settings
 // --------------------------------------------------------------------
 
-var code = config.code;
-
+var code   = config.code;
+var output = config.output;
+var uri    = config.config;
 
 // --------------------------------------------------------------------
 // Error Handler
@@ -39,7 +44,7 @@ gulp.task('pug', function buildHTML() {
 			errorHandler: onError
 		}))
 		.pipe(pug())
-		.pipe(gulp.dest(code.root))
+		.pipe(gulp.dest(output.root))
 		.pipe(browserSync.stream());
 });
 
@@ -59,23 +64,74 @@ gulp.task('sass', function() {
 			onError: browserSync.notify
 		}))
 		.pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}))
-		.pipe(gulp.dest(code.css))
+		.pipe(gulp.dest(output.cssOut))
 		.pipe(browserSync.stream());
 });
 
 
 // --------------------------------------------------------------------
+// Task: Image Compression
+// --------------------------------------------------------------------
+
+gulp.task('image', function() {
+	return gulp.src(code.img)
+		.pipe(plumber({
+			errorHandler: onError
+		}))
+		.pipe(image())
+		.pipe(gulp.dest(output.img));
+});
+
+// --------------------------------------------------------------------
+// Task: Move JS
+// --------------------------------------------------------------------
+
+gulp.task('js', function() {
+	return gulp.src(code.js)
+		.pipe(plumber({
+			errorHandler: onError
+		}))
+		.pipe(gulp.dest(output.js));
+});
+
+// --------------------------------------------------------------------
+// Task: Compress JS
+// --------------------------------------------------------------------
+
+gulp.task('compress-js', function() {
+	pump([
+        gulp.src(code.js),
+        uglify(),
+        gulp.dest(output.js)
+	    ],
+	    cb
+	);
+});
+
+// --------------------------------------------------------------------
+// Task: Compress CSS
+// --------------------------------------------------------------------
+
+gulp.task('compress-css', function() {
+	return gulp.src(output.cssIn)
+		.pipe(plumber({
+			errorHandler: onError
+		}))
+		.pipe(cleanCSS({compatibility: 'ie9'}))
+		.pipe(gulp.dest(output.cssOut));
+});
+
+// --------------------------------------------------------------------
 // Task: Browser Sync Server
 // --------------------------------------------------------------------
 
-gulp.task('serve', ['pug','sass'], function() {
+gulp.task('serve', ['pug', 'sass', 'image', 'js'], function() {
 	browserSync.init({
 		server: {
-			baseDir: code.root
+			baseDir: output.root
 		}
 	});
 });
-
 
 // --------------------------------------------------------------------
 // Task: Watch
@@ -83,13 +139,20 @@ gulp.task('serve', ['pug','sass'], function() {
 
 gulp.task('watch', function() {
 	gulp.watch(code.pug, ['pug']);
-	gulp.watch(code.sass, ['sass']);
-	gulp.watch(code.js).on('change', browserSync.reload);
+	gulp.watch([code.sass, code.sassDir], ['sass']);
+	gulp.watch(code.img, ['image']);
+	gulp.watch(output.php).on('change', browserSync.reload);
+	gulp.watch(code.js, ['js']).on('change', browserSync.reload);
 });
 
+// --------------------------------------------------------------------
+// Task: Build Project
+// --------------------------------------------------------------------
+
+gulp.task('build', ['compress-css', 'compress-js']);
 
 // --------------------------------------------------------------------
 // Task: Default
 // --------------------------------------------------------------------
 
-gulp.task('default', ['serve','watch']);
+gulp.task('default', ['serve', 'watch']);
