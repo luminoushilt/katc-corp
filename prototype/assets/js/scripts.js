60b96afff9c8004d8652d8f3bdfd681a43b84cf2
Hilton Williams <hwilliams@luminoushilt.com>
2019-09-27 11:34:10 -0700

    Revise folder structure.  Also remove css framework.

--- prototype/assets/js/scripts.js
+++ prototype/assets/js/scripts.js
@@ -1,25 +1,66 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
 
-  // Get all "navbar-burger" elements
-  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  const backToTopButton = document.querySelector('.back-to-top');
+  const scrollLinks = document.querySelectorAll('.scroll-link');
 
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
+  window.addEventListener('scroll', scrollFunction);
 
-    // Add a click event on each of them
-    $navbarBurgers.forEach(function ($el) {
-      $el.addEventListener('click', function () {
-
-        // Get the target from the "data-target" attribute
-        var target = $el.dataset.target;
-        var $target = document.getElementById(target);
-
-        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-
-      });
-    });
+  function scrollFunction() {
+    if(window.pageYOffset > 300) { // Show backToTopButton
+     backToTopButton.style.display = 'block';
+    }
+    else { // Hide backToTopButton
+     backToTopButton.style.display = 'none';
+    }
   }
 
-});
+  backToTopButton.addEventListener('click', smoothScrollBackToTop);
+
+  function smoothScrollBackToTop() {
+    const targetPosition = 0;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    const duration = 750;
+    let start = null;
+
+    window.requestAnimationFrame(step);
+
+    function step(timestamp) {
+      if (!start) start = timestamp;
+      const progress = timestamp - start;
+      window.scrollTo(0, distance*(progress/duration) + startPosition);
+      if (progress < duration) {
+        window.requestAnimationFrame(step);
+      }
+    }
+
+  }
+
+  for(let i=0; i<scrollLinks.length; i++) {
+    scrollLinks[i].addEventListener('click', smoothScroll);
+  }
+
+  // smooth scrolling
+  function smoothScroll(event) {
+    event.preventDefault();
+    const targetId = event.currentTarget.getAttribute('href');
+    const targetPosition = document.querySelector(targetId).offsetTop;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    const duration = 1000;
+    let start = null;
+
+    window.requestAnimationFrame(step);
+
+    function step(timestamp) {
+      if (!start) start = timestamp;
+      const progress = timestamp - start;
+      window.scrollTo(0, distance*(progress/duration) + startPosition);
+      if (progress < duration) {
+        window.requestAnimationFrame(step);
+      }
+    }
+
+  }
+
+ });
